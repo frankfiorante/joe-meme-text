fcffadc77ef06211e2c6de587b5614dae3ad8d52
Frank Fiorante <fiorantefrank@gmail.com>
2017-11-17 12:06:35 -0600

    first commit

---
+++ joeText.js
@@ -0,0 +1,18 @@
+function joeText() {
+    var word = document.getElementById('joetext').value;
+    var outputstring = "";
+    
+    for (i = 0; i < word.length; i++) {
+        if (!word[i].indexOf(' ') >= 0) {
+            outputstring += word[i] + " "; 
+        }
+        else if (/\s/.test(word)){
+            outputstring += '  ';
+        }
+    }
+    
+    outputstring = outputstring.toUpperCase().trim() + ".";
+    
+    alert(outputstring);
+    document.getElementById('translate').value = outputstring;
+}
